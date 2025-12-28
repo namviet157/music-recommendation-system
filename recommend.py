deb520ca63e17facaaafce91be9546fc487c4ca7
Nam Viet <buinamviet15705@gmail.com>
2025-12-28 23:21:36 +0700

    update

--- recommend.py
+++ recommend.py
@@ -1,8 +1,29 @@
-import joblib
+from huggingface_hub import hf_hub_download
+import pandas as pd
+from scipy.sparse import load_npz
+import faiss
 
-df = joblib.load('df_cleaned.pkl')
-X = joblib.load('tfidf_matrix.pkl')
-index = joblib.load('faiss_index.pkl')
+def load_models():
+    df_path = hf_hub_download(
+        repo_id="namviet157/music-recommendation",
+        filename="df_cleaned.parquet"
+    )
+    X_path = hf_hub_download(
+        repo_id="namviet157/music-recommendation",
+        filename="tfidf_matrix.npz"
+    )
+    index_path = hf_hub_download(
+        repo_id="namviet157/music-recommendation",
+        filename="faiss.index"
+    )
+
+    df = pd.read_parquet(df_path)
+    X = load_npz(X_path)
+    index = faiss.read_index(index_path)
+
+    return df, X, index
+
+df, X, index = load_models()
 
 def recommend_songs(song_name, df=df, top_k=5):
 
@@ -11,7 +32,7 @@ def recommend_songs(song_name, df=df, top_k=5):
         return "Song not found in the dataset."
     idx = idx[0]
 
-    query = X[idx].reshape(1, -1)
+    query = X[idx].toarray().astype('float32')
 
     scores, indices = index.search(query, top_k + 1)
 
